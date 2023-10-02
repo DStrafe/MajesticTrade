048f0e40fe31635d4db1ae1d3bd75d2e09a96ed9
Amwey <50361932+AmweyInc@users.noreply.github.com>
2023-10-02 14:28:32 +0300

    Add entities and settings for db

--- services/backend/api-user-service/src/main.ts
+++ services/backend/api-user-service/src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'node:path';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { USER_PACKAGE_NAME } from './User/User.pb';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        url: '0.0.0.0:50007',
+        package: [USER_PACKAGE_NAME], // AUTHORIZATION_SERVICE_NAME
+        protoPath: [
+          join(__dirname, '_proto/Auth/Auth._proto'),
+        ],
+        loader: {
+          keepCase: true,
+          longs: String,
+          enums: String,
+          defaults: true,
+          oneofs: true,
+        },
+      },
+    }
+  )
+
+  await app.listen();
 }
 bootstrap();
